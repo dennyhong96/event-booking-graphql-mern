8114df39e35f94810f268d2d9e6e16215fea0c62
Haiyang Hong <hong961127@gmail.com>
2020-08-03 03:53:28 -0700

    Setup events and createEvents type with DUMMY array

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const { buildSchema } = require("graphql");
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
       }
 
       schema {
@@ -24,11 +41,16 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Cooking", "Sailing", "Coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          ...args.eventInput,
+          price: +args.eventInput.price,
+          id: Math.random().toString(),
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,

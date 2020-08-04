c44b9d14f8ecdf38dccf9efd4482e4f88dd01267
Haiyang Hong <hong961127@gmail.com>
2020-08-04 05:14:32 -0700

    Add modal and backdrop

--- client/src/componnets/pages/Events.js
+++ client/src/componnets/pages/Events.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 
+import Modal from "../Modal/Modal";
+import Backdrop from "../Modal/Backdrop";
 import "./Events.css";
 
 const Events = () => {
   return (
-    <div className="events-control">
-      <p className="events-control__msg">Share your events.</p>
-      <button className="btn">Add Event</button>
-    </div>
+    <Fragment>
+      <div className="events-control">
+        <p className="events-control__msg">Share your events.</p>
+        <button className="btn">Add Event</button>
+      </div>
+      <Modal title="Add Event" canCancel canConfirm>
+        <p>Content</p>
+      </Modal>
+      <Backdrop />
+    </Fragment>
   );
 };
 

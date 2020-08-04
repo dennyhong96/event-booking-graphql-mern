ebdc34496b56fae2acccb17df99bfa3116473c89
Haiyang Hong <hong961127@gmail.com>
2020-08-03 23:50:12 -0700

    Setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import Auth from "./componnets/pages/Auth";
+import Events from "./componnets/pages/Events";
+import Bookings from "./componnets/pages/Bookings";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact from="/" to="/auth" />
+        <Route exact path="/auth" component={Auth} />
+        <Route exact path="/events" component={Events} />
+        <Route exact path="/bookings" component={Bookings} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
